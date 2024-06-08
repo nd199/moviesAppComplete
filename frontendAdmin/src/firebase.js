fd5b49927e360cff59adcb641f767a2afd26c4f3
Narendran <naren06251999@gmail.com>
2024-06-08 21:18:16 +0530

    Worked on frontend Part added admin Dashboard from previous proj need to modify further and work on pages interactivity on diff screen

---
+++ frontendAdmin/src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDkH3YGCEHByT4yKJCLf_v53WBdIX9RuU",
+  authDomain: "shop-17181.firebaseapp.com",
+  projectId: "shop-17181",
+  storageBucket: "shop-17181.appspot.com",
+  messagingSenderId: "657091532528",
+  appId: "1:657091532528:web:2d4b57bc997ce5a3735435",
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export default app;
