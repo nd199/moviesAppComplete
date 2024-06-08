b882c4507a492f2bd57a6f86ccf2c2fd2f8eebd7
Narendran <naren06251999@gmail.com>
2024-06-08 21:18:16 +0530

    Worked on frontend Part added admin Dashboard from previous proj need to modify further and work on pages interactivity on diff screen

--- frontend/src/Components/Featured.js
+++ frontend/src/Components/Featured.js
@@ -15,16 +15,18 @@ const Featured = ({type}) => {
 
     return (
         <div className="featured">
-            <div className="video">
-                <VideoComponent
-                    className="videoFeature"
-                    filePath={filepath}
-                    isMuted={isMuted}
-                />
-            </div>
-            <div className="info">
-                <h1>John Wick 4 (2023)</h1>
-                <span className="desc">
+            <div className="slider-container">
+                <div className="sliderWrapper">
+                    <div className="video">
+                        <VideoComponent
+                            className="videoFeature"
+                            filePath={filepath}
+                            isMuted={isMuted}
+                        />
+                    </div>
+                    <div className="info">
+                        <h1>John Wick 4 (2023)</h1>
+                        <span className="desc">
                     An action-packed thriller that follows the legendary hitman John
                     Wick (Keanu Reeves) as he continues his relentless quest for
                     freedom. With the bounty on his head ever-increasing, Wick faces his
@@ -34,19 +36,21 @@ const Featured = ({type}) => {
                     assassins. Directed by Chad Stahelski, this installment expands the
                     franchise's mythology and delivers exhilarating action and suspense.
                 </span>
-                <div className="buttons">
-                    <button className="watchBtn">
-                        <PlayArrowOutlined/>
-                        Watch
-                    </button>
-                    <button className="watchListBtn">
-                        <AddToQueueOutlined/>
-                        Add to WatchList
-                    </button>
-                    <button className="audioBtn" onClick={() => toggleAudioHandler()}>
-                        {isMuted ? <VolumeOff/> : <VolumeUp/>}
-                        {isMuted ? "UnMute" : "Mute"}
-                    </button>
+                        <div className="buttons">
+                            <button className="watchBtn">
+                                <PlayArrowOutlined/>
+                                Watch
+                            </button>
+                            <button className="watchListBtn">
+                                <AddToQueueOutlined/>
+                                Add to WatchList
+                            </button>
+                            <button className="audioBtn" onClick={() => toggleAudioHandler()}>
+                                {isMuted ? <VolumeOff/> : <VolumeUp/>}
+                                {isMuted ? "UnMute" : "Mute"}
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
