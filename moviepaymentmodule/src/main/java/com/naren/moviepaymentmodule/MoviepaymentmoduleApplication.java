94595ae221111e7a722cad969614d19f45c876d2
Narendran <naren06251999@gmail.com>
2024-07-03 12:55:18 +0530

    Payment module start

---
+++ moviepaymentmodule/src/main/java/com/naren/moviepaymentmodule/MoviepaymentmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.naren.moviepaymentmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviepaymentmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviepaymentmoduleApplication.class, args);
+	}
+
+}
---
+++ moviepaymentmodule/src/test/java/com/naren/moviepaymentmodule/MoviepaymentmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naren.moviepaymentmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviepaymentmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
