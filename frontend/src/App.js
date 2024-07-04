7841001187d974249d412da6b4fb30815d5e5c0c
Narendran <naren06251999@gmail.com>
2024-07-04 19:53:14 +0530

    Payment node module Initial push 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,40 +5,43 @@ import Movies from "./Pages/Movies";
 import Shows from "./Pages/Shows";
 import VideoFullScreen from "./Pages/VideoFullScreen";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import RegistrationForm from "./Pages/RegistrationForm";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import LoginForm from "./Pages/LoginForm";
+import Subscription from "./Pages/Subscription";
 
 function AppWithNavigation() {
-  const user = useSelector((state) => state.user);
-  const isRegistered = user?.currentUser?.isRegistered || false;
-  const isLoggedIn = user?.currentUser?.customerDTO?.isLogged || false;
+    const user = useSelector((state) => state.user);
+    const currentUser = user?.currentUser;
+    const isRegistered = currentUser?.isRegistered || false;
+    const isLoggedIn = currentUser?.customerDTO?.isLogged || false;
 
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route
-            path="/"
-            element={isRegistered ? <Home /> : <RegistrationForm />}
-          />
-          <Route
-            path="/Login"
-            element={isLoggedIn ? <Home /> : <LoginForm />}
-          />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/shows" element={<Shows />} />
-          <Route path="/vfs" element={<VideoFullScreen />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={isRegistered ? <Home/> : <RegistrationForm/>}
+                    />
+                    <Route
+                        path="/Login"
+                        element={isLoggedIn ? <Home/> : <LoginForm/>}
+                    />
+                    <Route path="/about" element={<AboutUs/>}/>
+                    <Route path="/movies" element={<Movies/>}/>
+                    <Route path="/shows" element={<Shows/>}/>
+                    <Route path="/vfs" element={<VideoFullScreen/>}/>
+                    <Route path="/subscribe" element={<Subscription/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 function App() {
-  return <AppWithNavigation />;
+    return <AppWithNavigation/>;
 }
 
 export default App;
