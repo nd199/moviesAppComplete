48a3720bd2161b3a5e781d5dcd3856e39e7ce6e8
Narendran M.D <narendranm.d.@Narendrans-MacBook-Air.local>
2025-03-28 17:38:10 +0530

    Nav-mobile-Ready

--- frontend/src/Components/Featured.js
+++ frontend/src/Components/Featured.js
@@ -10,6 +10,7 @@ const filepath =
 const Featured = () => {
     const [isMuted, setIsMuted] = useState(true);
     const [isLoading, setIsLoading] = useState(true);
+    const [more, setMore] = useState(false);
 
     const toggleAudioHandler = () => {
         setIsMuted(!isMuted);
@@ -37,30 +38,60 @@ const Featured = () => {
                         />
                     </div>
                     <div className="info">
-                        <h1>John Wick 4 (2023)</h1>
+                        <div className="info-top">
+                            <h1>John Wick 4 (2023)</h1>
+                            {more && <button
+                                style={{
+                                    width: "15px",
+                                    height: "15px",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                }}
+                                onClick={() => setMore(!more)}>X
+                            </button>}
+                        </div>
                         <span className="desc">
-              An action-packed thriller that follows the legendary hitman John
-              Wick (Keanu Reeves) as he continues his relentless quest for
-              freedom. With the bounty on his head ever-increasing, Wick faces
-              his most formidable foes yet, traveling from New York to Paris,
-              Osaka, and Berlin. The film features intense combat sequences,
-              stunning choreography, and an exploration of the deep underworld
-              of assassins. Directed by Chad Stahelski, this installment expands
-              the franchise's mythology and delivers exhilarating action and
-              suspense.
-            </span>
+                              An action-packed thriller that follows the legendary hitman John
+                              Wick (Keanu Reeves) as he continues his relentless quest for
+                              freedom. With the bounty on his head ever-increasing, Wick faces
+                              his most formidable foes yet, traveling from New York to Paris,
+                              Osaka, and Berlin. The film features intense combat sequences,
+                              stunning choreography, and an exploration of the deep underworld
+                              of assassins. Directed by Chad Stahelski, this installment expands
+                              the franchise's mythology and delivers exhilarating action and
+                              suspense.
+                        </span>
+                        {more && <span className="desc-more">
+                              An action-packed thriller that follows the legendary hitman John
+                              Wick (Keanu Reeves) as he continues his relentless quest for
+                              freedom. With the bounty on his head ever-increasing, Wick faces
+                              his most formidable foes yet, traveling from New York to Paris,
+                              Osaka, and Berlin. The film features intense combat sequences,
+                              stunning choreography, and an exploration of the deep underworld
+                              of assassins. Directed by Chad Stahelski, this installment expands
+                              the franchise's mythology and delivers exhilarating action and
+                              suspense.
+                        </span>}
+                        {!more && <span className="short-desc" style={{display: "none"}}>
+                            An action-packed thriller that follows the legendary hitman John
+                            Wick (Keanu Reeves) as he continues his relentless quest for
+                            freedom. <button onClick={() => setMore(true)}>More...</button>
+                        </span>}
                         <div className="buttons">
                             <button className="watchBtn">
                                 <PlayArrowOutlined/>
-                                Watch
+                                <p className="btns-text">Watch</p>
                             </button>
                             <button className="watchListBtn">
                                 <AddToQueueOutlined/>
-                                Add to WatchList
+                                <p className="btns-text"><span className="sm-screen">Add to</span> WatchList</p>
                             </button>
-                            <button className="audioBtn" onClick={() => toggleAudioHandler()}>
+                            <button className="audioBtn"
+                                    onClick={() => toggleAudioHandler()}>
                                 {isMuted ? <VolumeOff/> : <VolumeUp/>}
-                                {isMuted ? "UnMute" : "Mute"}
+                                {isMuted ? <p className="btns-text">UnMute</p> :
+                                    <p className="btns-text">Mute</p>}
                             </button>
                         </div>
                     </div>
