51541ab13f6a7e3b122a60f404e8e261137573b3
Narendran <narendran@Narendrans-MacBook-Air.local>
2026-02-10 19:52:53 +0530

    chore(repo): fix removed the frontends that are not needed anymore since merged

--- frontendpayment/src/Network/AxiosMethods.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const paymentRequest = axios.create({
-  baseURL: "https://movie-payment.onrender.com",
-});
-
-export const springRequest = axios.create({
-  baseURL: "https://movieticket-api.onrender.com",
-});
--- rev.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("btn");
-const text = document.getElementById("text");
-
-button.addEventListener("click", () => {
-  text.innerText =
-    text.innerText === "Hello Accenture" ? "Welcome ASE" : "Hello Accenture";
-});
